9e8bad8f40e732bab995a3b9bd516a899042a71b
Ben Highsted <49290301+BenHighsted@users.noreply.github.com>
2020-04-18 02:17:16 +1200

    Now calls a method on user input
    Also changed some of the look. This means pretty much everything other than the conversion code is completed (Other than styling the website neatly).

--- converter.js
+++ converter.js
@@ -1,3 +1,11 @@
-function ConvertTextToMorse(){
+function ConvertTextToMorse()
+{
 
+}
+
+//Rather than simply printing the text, I will run it through a method that does the conversion, then displays said converted text.
+function textChanged()
+{
+    var x = document.getElementById("inputBox").value;
+    document.getElementById("output").innerHTML = "You wrote: " + x;
 }
